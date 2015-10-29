bc978a40bee0fe6dc9f227095b7b1eaae7178022
touhonoob <touhonoob@gmail.com>
2015-10-29 23:51:06 +0800

    set hosts

---
+++ .travis/pre.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cat << EOF >> /etc/hosts
+api.sandbox.crowdsdom.com 120.25.13.59
+account.sandbox.crowdsdom.com 120.25.13.59
+api.crowdsdom.com 120.25.13.59
+account.crowdsdom.com 120.25.13.59
+EOF
