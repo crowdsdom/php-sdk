07e7f1f53c22562ea492ef8f8c08e9fddd4d47d0
touhonoob <touhonoob@gmail.com>
2015-11-08 14:31:41 +0800

    add job.jobType

--- src/Labor/Models/Job.php
+++ src/Labor/Models/Job.php
@@ -11,4 +11,10 @@ class Job extends Base\Job
         $this->response = $this->client->getGuzzle()->request('GET', static::ENDPOINT . "/{$id}/tasks", []);
         return json_decode($this->response->getBody()->getContents(), true);
     }
+
+    public function jobType($id)
+    {
+        $this->response = $this->client->getGuzzle()->request('GET', static::ENDPOINT . "/{$id}/_jobType", []);
+        return json_decode($this->response->getBody()->getContents(), true);
+    }
 }
--- tests/Integration/Labor/Models/JobTest.php
+++ tests/Integration/Labor/Models/JobTest.php
@@ -9,12 +9,28 @@ class JobTest extends ModelTest
     protected $model = Job::class;
     protected $data = 'job';
 
+    protected $job;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->job = new Job($this->client);
+    }
+
     public function testTasks()
     {
-        $job = new Job($this->client);
-        $tasks = $job->tasks($this->createModel['id']);
+        $tasks = $this->job->tasks($this->createModel['id']);
         $this->assertInternalType('array', $tasks);
-        $this->assertEquals(200, $job->getResponse()->getStatusCode());
+        $this->assertEquals(200, $this->job->getResponse()->getStatusCode());
+    }
+
+    public function testJobType()
+    {
+        $this->markTestSkipped('job.jobType not implemented');
+
+        $jobType = $this->job->jobType($this->createModel['id']);
+        $this->assertInternalType('array', $jobType);
+        $this->assertEquals(200, $this->job->getResponse()->getStatusCode());
     }
 
 }
